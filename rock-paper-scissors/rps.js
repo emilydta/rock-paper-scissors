8c1bc078c2bf881c0951ec1f38224965632dca28
Emily <emilydta@users.noreply.github.com>
2021-11-24 10:32:22 +1100

    moved vars to top of page, added result mssg, clrs

--- rock-paper-scissors/rps.js
+++ rock-paper-scissors/rps.js
@@ -1,6 +1,27 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const computerScoreHeading = document.querySelector('#computer-score-heading');
+const computerScoreNumber = document.querySelector('#computer-score-number');
+
+const playerScoreHeading = document.querySelector('#player-score-heading');
+const playerScoreNumber = document.querySelector('#player-score-number');
+
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => playRound("Rock", computerPlay()));
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => playRound("Paper", computerPlay()));
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => playRound("Scissors", computerPlay()));
+
+const resultMessage = document.createElement('p');
+resultMessage.setAttribute("id", "result-message");
+middle.appendChild(resultMessage);
+
+
 const optionsArray = ["Rock", "Paper", "Scissors"];
 
 function chooseRandomElement(arr) {
@@ -17,8 +38,14 @@ function playRound(playerSelection, computerSelection = computerPlay()) {
     console.log("You: " + playerSelection);
 
     if (!optionsArray.includes(playerSelection)) {
+        resultMessage.innerText = "Error. You did not choose Rock, Paper, or Scissors.";
         return "Error. You did not choose Rock, Paper, or Scissors.";
     } else if (playerSelection == computerSelection) {
+        resultMessage.innerText = "It's a tie!";
+        playerScoreHeading.classList.remove("score-color");
+        playerScoreNumber.classList.remove("score-color");
+        computerScoreHeading.classList.remove("score-color");
+        computerScoreNumber.classList.remove("score-color");
         return "It's a tie!";
     }
 
@@ -28,31 +55,51 @@ function playRound(playerSelection, computerSelection = computerPlay()) {
     
     if (playerWins) {
         ++playerScore;
+        playerScoreNumber.innerText = `${playerScore}`
+        playerScoreHeading.classList.add("score-color");
+        playerScoreNumber.classList.add("score-color");
+
+        computerScoreHeading.classList.remove("score-color");
+        computerScoreNumber.classList.remove("score-color");
+
+        document.getElementsByClassName(middle);
+
+        resultMessage.innerText = `You Win! ${playerSelection} beats ${computerSelection}!`;
+        
+
+        
         return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        
     } else {
+
         ++computerScore;
+        
+        computerScoreNumber.innerText = `${computerScore}`;
+        computerScoreHeading.classList.add("score-color");
+        computerScoreNumber.classList.add("score-color");
+
+      
+        playerScoreHeading.classList.remove("score-color");
+        playerScoreNumber.classList.remove("score-color");
+
+        resultMessage.innerText = `You Lose! ${computerSelection} beats ${playerSelection}!`;
+        
         return `You Lose! ${computerSelection} beats ${playerSelection}!`;
     }
 }
 
-/**function game() {
-    let i = 1;
-    while (i <= 5) {
+function game() {
+    /**let i = 1;
+   /**  while (i <= 5) { 
         const userInput = prompt("Rock, Paper, or Scissors?");
-        console.log(playRound(userInput));
+        console.log(playRound(userInput));**/
         console.log("Computer: " + computerScore);
         console.log("You: " + playerScore);
-        ++i;
-    }
-    return "END";
-}**/
-
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', () => playRound("Rock", computerPlay()));
-
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', () => playRound("Paper", computerPlay()));
+        /**++i;**/
+    
+    return;
+}
 
 
-const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', () => playRound("Scissors", computerPlay()));
+
+
